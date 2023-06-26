f28848342a64ac7ab7c32a6de719012fa003cb3b
AKASH H N <akashgowda0902@gmail.com>
2023-06-26 10:20:47 +0530

    booking , transactions, passengers models completed

---
+++ booking_service/booking/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render
+from .models import *
+from .serializer import *
+from rest_framework.decorators import api_view
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import  ListModelMixin,RetrieveModelMixin,DestroyModelMixin,CreateModelMixin,UpdateModelMixin
+from rest_framework.response import Response
+from rest_framework import status
+
+# -------------------------------------Transaction Operations--------------------------------------
+
+class TransactionInsertandGettingall(GenericAPIView,CreateModelMixin,ListModelMixin):
+     queryset = Transaction.objects.all()
+     serializer_class=TransactionSerializer
+     def post(self,request):
+          return self.create(request)
+     def get(self,request):
+        return self.list(request)
+
+class TransactionUpadateAndDeleteAndRetraiveByID(GenericAPIView,UpdateModelMixin,DestroyModelMixin,RetrieveModelMixin):
+     queryset=Transaction.objects.all()
+     serializer_class=TransactionSerializer
+     def put(self,request,**kwargs):
+          return self.update(request,**kwargs)
+     def delete(self,request,**kwargs):
+          return self.destroy(request,**kwargs)
+     def get(self,request,**kwargs):
+          return self.retrieve(request,**kwargs)
+     
+@api_view(['GET'])
+def getTransactionByBookingId(request, bookingId):
+     transaction = Transaction.objects.filter(booking=bookingId)
+     transactionSerializer = TransactionSerializer(instance = transaction)
+     return Response(transactionSerializer.data, status=status.HTTP_200_OK)
+     
+# -------------------------------------Passengers Operations--------------------------------------
+
+class PassengerInsertandGettingall(GenericAPIView,CreateModelMixin,ListModelMixin):
+     queryset = Passenger.objects.all()
+     serializer_class=PassengerSerializer
+     def post(self,request):
+          return self.create(request)
+     def get(self,request):
+        return self.list(request)
+     
+class PassengerUpadateAndDeleteAndRetraiveByID(GenericAPIView,UpdateModelMixin,DestroyModelMixin,RetrieveModelMixin):
+     queryset=Passenger.objects.all()
+     serializer_class=PassengerSerializer
+     def put(self,request,**kwargs):
+          return self.update(request,**kwargs)
+     def delete(self,request,**kwargs):
+          return self.destroy(request,**kwargs)
+     def get(self,request,**kwargs):
+          return self.retrieve(request,**kwargs)
+     
+@api_view(['GET'])
+def getPassengerByBookingId(request, bookingId):
+     passengers = Passenger.objects.filter(booking=bookingId)
+     passengerSerializer = PassengerSerializer(data = passengers, many=True)
+     passengerSerializer.is_valid()
+     return Response(passengerSerializer.data, status=status.HTTP_200_OK)
+# -------------------------------------Booking Operations--------------------------------------
+
+@api_view(['POST','GET'])
+def booking(request):
+     if request.method == 'POST':
+          serializer = BookingSerializer(data = request.data)
+          if serializer.is_valid():
+               serializer.save()
+               booking_id = serializer.instance.bookingId
+               bookings = Booking.objects.get(bookingId = booking_id)
+               retriveSerializer = BookingRetrieveSerializer(instance = bookings)
+               transaction = Transaction.objects.get(booking = booking_id)
+               transactionSerializer = TransactionSerializer(instance = transaction)
+               passengers = Passenger.objects.filter(booking=booking_id)
+               passengerSerializer = PassengerSerializer(data = passengers, many=True)
+               passengerSerializer.is_valid()
+               return Response([retriveSerializer.data,transactionSerializer.data,passengerSerializer.data], status= status.HTTP_201_CREATED)
+          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     else:
+          booking = Booking.objects.all()
+          serializer = BookingRetrieveSerializer(data=booking, many=True)
+          serializer.is_valid()
+          print(serializer.data)
+          return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['PUT','GET','DELETE'])
+def getBooking(request,id):
+     if request.method =='PUT':
+          booking = Booking.objects.get(bookingId=id)
+          serializer = BookingRetrieveSerializer(booking, data=request.data)
+          if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data)
+          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     elif request.method =='GET':
+          booking = Booking.objects.get(bookingId=id)
+          serializer = BookingRetrieveSerializer(instance = booking)
+          return Response(serializer.data, status=status.HTTP_200_OK)
+     else:
+          booking = Booking.objects.get(bookingId=id)
+          transaction = Transaction.objects.get(booking = id)
+          transaction.delete()
+          passengers = Passenger.objects.filter(booking= id)
+          for passenger in passengers:
+               passenger.delete()
+          booking.delete()
+          return Response(status=status.HTTP_204_NO_CONTENT)
--- booking_service/booking_service/settings.py
+++ booking_service/booking_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'booking',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'booking_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'booking-module',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
